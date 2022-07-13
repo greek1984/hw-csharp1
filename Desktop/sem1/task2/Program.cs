0afdd5477a39ff99476c39bb2a23f495fca5cabb
greek <greek101284@gmail.com>
2022-07-14 00:15:21 +0800

    made 2nd task

---
+++ Desktop/sem1/task2/Program.cs
@@ -0,0 +1,18 @@
+﻿{
+
+// ввод 2-х чисел
+Console.WriteLine("Введите 1-е число");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите 2-е число");
+int numberB = Convert.ToInt32(Console.ReadLine());
+// условие сравнения
+if (numberA < numberB)
+{    
+    Console.Write($"max = {numberB}");
+}
+else     
+{
+    Console.Write($"max = {numberA}");
+}
+
+}

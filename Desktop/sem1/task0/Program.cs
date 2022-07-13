b5b2bf96df3f0ff072d7f238df6adc42db8e6d68
greek <greek101284@gmail.com>
2022-07-14 00:07:49 +0800

    made task0

---
+++ Desktop/sem1/task0/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Введите число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+int result = numberA * numberA;
+Console.Write("Получите квадрат числа: ");
+Console.WriteLine(result);  

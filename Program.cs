3875e5c4e14f6c6ebb35828aae5e294c1978cd18
greek1984 <83595490+greek1984@users.noreply.github.com>
2022-07-14 00:29:01 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿// ввод числа
+Console.WriteLine("Введите число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int number = 2;
+// условие
+    while(number <= N) 
+    {
+        Console.Write(number + ",");
+        number += 2;
+    }
+    if (N < 0)
+    {
+        Console.WriteLine("Ввдите положительное число!");
+    }
+Console.WriteLine("\b.");
+    
+

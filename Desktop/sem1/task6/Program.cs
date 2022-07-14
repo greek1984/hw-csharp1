c9327d5074f1ee661e88737287cc0f8110711a8e
greek <greek101284@gmail.com>
2022-07-14 08:23:47 +0800

    made 6th task

---
+++ Desktop/sem1/task6/Program.cs
@@ -0,0 +1,16 @@
+﻿{
+
+// вводное число
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+// условие проверки
+    if (number % 2 == 0)
+    {
+        Console.WriteLine($"{number} - чётное число");
+    }
+    else
+    {
+        Console.WriteLine($"{number} - нечётное число");
+    }
+
+}    

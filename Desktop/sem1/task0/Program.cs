a9ac2db40f8f3e537fe9b68947a24e0a048ed1d2
greek1984 <83595490+greek1984@users.noreply.github.com>
2022-07-14 08:31:22 +0800

    Delete Desktop/sem1/task0 directory

--- Desktop/sem1/task0/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿{
-
-Console.WriteLine("Введите число: ");
-int numberA = Convert.ToInt32(Console.ReadLine());
-int result = numberA * numberA;
-Console.Write("Получите квадрат числа: ");
-Console.WriteLine(result);  
-
-}

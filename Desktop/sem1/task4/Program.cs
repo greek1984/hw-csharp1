f34cd015fa22bcb9bc0080c248973c0784750460
greek <greek101284@gmail.com>
2022-07-14 08:22:11 +0800

    made task4

---
+++ Desktop/sem1/task4/Program.cs
@@ -0,0 +1,17 @@
+﻿{
+    // ввод данных
+    Console.WriteLine("Введите 1-е число: ");
+    int numberA = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите 2-е число: ");
+    int numberB = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите 3-е число: ");
+    int numberC = Convert.ToInt32(Console.ReadLine());
+    int max = numberA;
+    // условие
+    if (numberA > max) max = numberA;
+    if (numberB > max) max = numberB;
+    if (numberC > max) max = numberC;
+    Console.Write("Ответ:");
+    Console.WriteLine(max);  
+                   
+}
